25211e2a2de1ee74876f3c46d1a914bfc18b6291
writetoritika <writetoritika@gmail.com>
2023-12-25 15:16:43 -0600

    Made Changes

--- dev/level2/backend.tf
+++ dev/level2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "israel-terraform"
     key            = "level2-dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "israel-dynamo-terraform"
   }
 }
