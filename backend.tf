1fdf9506c1f562264c79b507ada0d61ff4064131
writetoritika <writetoritika@gmail.com>
2023-12-27 00:08:51 -0600

    Changes to S3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "israel-terraform"
+    bucket         = "ritika-terraform"
     key            = "level2-prod/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "writetoritika-dynamo-terraform"
