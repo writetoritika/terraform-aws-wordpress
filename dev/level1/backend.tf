41d8a2561286fa464c155133a3c2d85344578560
writetoritika <writetoritika@gmail.com>
2024-01-03 13:43:20 -0600

    Change in Bucket Name

--- dev/level1/backend.tf
+++ dev/level1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "israel-terraform"
+    bucket         = "ritika-terraform"
     key            = "level1-dev/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "ritika-dynamo-terraform"
